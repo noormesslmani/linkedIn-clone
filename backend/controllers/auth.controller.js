db745119dab5e2d1ff8d7cbb50ce9dd7a83731e7
noormesslmani <noor.messlmani@gmail.com>
2022-10-15 10:26:56 +0300

    Backend: created login route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,23 @@ const User = require('../models/users.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const login = async (req, res)=>{
+    const {email, password} = req.body;
+    
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user) return res.status(404).json({message: "Invalid Credentials"});
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+    const token = jwt.sign({email: user.email, name: user.name, userType: 1}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '1h'
+    });
+    res.status(200).json(token)
+}
+
+
 const signup = async (req, res)=>{
     const {first_name, last_name, email, password} = req.body;
     try{
@@ -21,5 +38,6 @@ const signup = async (req, res)=>{
 }
 
 module.exports = {
-    signup
+    signup,
+    login
 }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,6 +3,6 @@ const {login, signup} = require('../controllers/auth.controller')
 const router = Router();
 
 router.post('/signup', signup);
-
+router.post('/login', login);
 
 module.exports = router;
