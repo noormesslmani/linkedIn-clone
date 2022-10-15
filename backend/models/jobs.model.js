b2903e1c35fa405eb3d4d95b3713bccec983cb9d
noormesslmani <noor.messlmani@gmail.com>
2022-10-15 21:08:57 +0300

    Backend: created jobs model

---
+++ backend/models/jobs.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: 'name is required'
+    },
+    company: [{
+        company_id:{
+            type: String,
+            required: true
+        },
+        comany_name:{
+            type: String,
+            required: true
+        }
+    }],
+    job_type: {
+        type: String,
+        required: 'job_type is required',
+    },
+    experience: {
+        type: String,
+        required: 'experience is required',
+    },
+    details: {
+        type: String,
+        required: 'details is required',
+    },
+})
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
