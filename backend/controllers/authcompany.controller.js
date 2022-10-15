d6a103edc462d86807137c33084ae8cde7b225db
noormesslmani <noor.messlmani@gmail.com>
2022-10-15 11:04:15 +0300

    Backend: finished signupCompany function

--- backend/controllers/authcompany.controller.js
+++ backend/controllers/authcompany.controller.js
@@ -1,16 +1,19 @@
 const Company = require('../models/companies.model');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 const signupCompany = async (req, res)=>{
-    const { email, password} = req.body;
+    const { email, password, name, city, country} = req.body;
     try{
         const company = new Company();
+        company.name = name;
+        company.city = city;
+        company.country = country;
         company.email = email;
         company.password = await bcrypt.hash(password, 10);
 
-        await user.save();
-        res.json(user)
+        await company.save();
+        res.json(company)
     }catch(err){
         res.status(400).json({
             message: err.message
