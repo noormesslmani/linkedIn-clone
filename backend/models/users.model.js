4a96a1ae6e7fc9e0b61362c0ac548e3dbc7f2d2d
noormesslmani <noor.messlmani@gmail.com>
2022-10-15 10:19:33 +0300

    Backend: added more attributes to users model

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -23,7 +23,75 @@ const userSchema = new mongoose.Schema({
         required: 'password is required',
         select: false
     },
-
+    headers: {
+        type: String,
+        default: null
+    },
+    profile_picture: {
+        type: String,
+        default: null
+    },
+    cover_photo: {
+        type: String,
+        default: null
+    },
+    country: {
+        type: String,
+        default: null
+    },
+    city: {
+        type: String,
+        default: null
+    },
+    experience: [{
+        title: {
+            type: String,
+        },
+        employment_type: {
+            type: String,
+        },
+        company_name: {
+            type: String,
+        },
+        location: {
+            type: String,
+        },
+        start_date: {
+            type: Date,
+        },
+        end_date: {
+            type: Date,
+        },
+        industry: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+    }],
+    eduction: [{
+        school: {
+            type: String,
+        },
+        degree: {
+            type: String,
+        },
+        field: {
+            type: String,
+        },
+        start_date: {
+            type: Date,
+        },
+        end_date: {
+            type: Date,
+        },
+        grade: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+    }]
 })
 
 const User = mongoose.model('User', userSchema);
