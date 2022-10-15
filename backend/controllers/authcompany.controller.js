09fdc7a34bac60e449f5cadbccd83e262f449ae9
noormesslmani <noor.messlmani@gmail.com>
2022-10-15 10:52:01 +0300

    Backend: mdified the request body of signupCompany function

--- backend/controllers/authcompany.controller.js
+++ backend/controllers/authcompany.controller.js
@@ -3,13 +3,9 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const signupCompany = async (req, res)=>{
-    const {name, country, city, email, password, about} = req.body;
+    const { email, password} = req.body;
     try{
         const company = new Company();
-        company.name = name;
-        company.country = country;
-        company.city = city;
-        company.about = about;
         company.email = email;
         company.password = await bcrypt.hash(password, 10);
 
