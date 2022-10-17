1c499da6f806b33583cdcad5b07e9e90bfd4b6c5
noormesslmani <noor.messlmani@gmail.com>
2022-10-17 14:06:09 +0300

    Frontend: displayed experience, education, skills in profile page

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -8,10 +8,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import cover from '../assets/bg.webp'
 import profile from '../assets/blank-profile.webp'
+import image from '../assets/original.png';
 function UserProfile(){
     const info = JSON.parse(localStorage.me)
     const experience= info.experience
     const education = info.education
+    const skills= info.skills
+
+    const handleEdu=()=>{
+
+    }
+    const handleExp=()=>{}
+    const handleSkills=()=>{}
     return (
         <>
             <NavUser/>
@@ -29,20 +37,48 @@ function UserProfile(){
                 <div className='experience'>
                     <div className='experience-row'>
                         <h3>Experience</h3> 
-                        <FontAwesomeIcon icon={faCirclePlus} className='add-experience' />
+                        <FontAwesomeIcon icon={faCirclePlus} className='add-experience' onClick={handleExp}/>
                     </div>
+                    {experience.map((exp)=>
+                        <div className='exp-details'>
+                            <img src={image} className='job-img' ></img>
+                            <div className='written-details'>
+                                <h4>{exp.title}</h4>
+                                <p className='detail'>{exp.company_name}</p>
+                                <p className='detail'>{exp.start_date}</p>
+                                <p className='detail'>{exp.end_date}</p>
+                                <p className='detail'>{exp.location}</p>
+                            </div>
+                        </div>
+                    )}
+                
                 </div>
                 <div className='education'>
                     <div className='experience-row'>
                         <h3>Education</h3> 
-                        <FontAwesomeIcon icon={faCirclePlus} className='add-experience' />
+                        <FontAwesomeIcon icon={faCirclePlus} className='add-experience' onClick={handleEdu}/>
                     </div>
+                    {education.map((edu)=>
+                        <div className='exp-details'>
+                            <img src={image} className='job-img' ></img>
+                            <div className='written-details'>
+                                <h4>{edu.school}</h4>
+                                <p className='detail'>{edu.degree}</p>
+                                <p className='detail'>{edu.field}</p>
+                            </div>
+                        </div>
+                    )}
                 </div>
                 <div className='skills'>
                     <div className='experience-row'>
                         <h3>Skills</h3> 
-                        <FontAwesomeIcon icon={faCirclePlus} className='add-experience' />
+                        <FontAwesomeIcon icon={faCirclePlus} className='add-experience' onClick={handleSkills}/>
                     </div>
+                    {skills.map((skill)=>
+                        <div className='written-details'>
+                            <h4>{skill}</h4>
+                        </div>
+                    )}
                 </div>
              </div>
             </section>
