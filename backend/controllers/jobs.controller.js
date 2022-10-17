128d036eeeeffb192f04c1e11b8cce771a220a6b
noormesslmani <noor.messlmani@gmail.com>
2022-10-17 08:53:11 +0300

    Backend: returned company info with each job

--- backend/controllers/jobs.controller.js
+++ backend/controllers/jobs.controller.js
@@ -3,7 +3,7 @@ const Job = require('../models/jobs.model');
 
 
 const getAllJobs= async (req, res) => {
-    Job.find()
+    Job.find().populate('company')
     .then((job)=>res.status(200).json(job))
     .catch((err)=>res.status(400).json(err))
 };
